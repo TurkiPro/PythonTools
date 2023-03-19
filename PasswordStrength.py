a0bb21a2a1e73388f23c1fbe080e21cdbeda988c
Turki Almalki <iiata767@gmail.com>
2023-03-19 21:32:10 +0300

    Update PasswordStrength.py
    Added a check mechanism to see if the password has been compromised

--- PasswordStrength.py
+++ PasswordStrength.py
@@ -1,4 +1,6 @@
 import re
+import hashlib
+import requests
 
 def password_check(password):
     strength = 0
@@ -14,14 +16,33 @@ def password_check(password):
         strength += 1
     return strength
 
+def pwned_api_check(password):
+    sha1_password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    head = sha1_password[:5]
+    tail = sha1_password[5:]
+    url = 'https://api.pwnedpasswords.com/range/'  + head
+    resource = requests.get(url)
+    hashes = (line.split(':') for line in resource.text.splitlines())
+    
+    for i, count in hashes:
+        if i == tail:
+            return count
+    return 0
+
     
 password = input("Enter your password: ")
 
 strength = password_check(password)
+compromised = pwned_api_check(password)
 
 print(f"your password has a strength of {strength} out of 5")
 
-if strength < 5:
-    print("Consider the following:\n-Password length should be more than 12 characters\n-It needs to have at least one lower-case and upper-case letter\n-It needs to have one number\n-It needs to have one special character")
+if compromised:
+    print(f"\n{compromised} breaches found for this password. You should change it.\n")
 else:
-    print("Nice!! your password is strong.")
+    print("\nNo breaches found for this password.\n")
+
+if strength < 5:
+    print("\nConsider the following:\n-Password length should be more than 12 characters\n-It needs to have at least one lower-case and upper-case letter\n-It needs to have one number\n-It needs to have one special character\n")
+else:
+    print("\nNice!! your password is strong.\n")
